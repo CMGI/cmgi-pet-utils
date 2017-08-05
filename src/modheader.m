3336b8ebb3a2db6aaac73b2a60865c1a6708db2b
Zachary T. Harmany <zharmany@gmail.com>
2017-08-05 00:35:36 -0500

    Updated help for modheader, added optional arguments.

--- src/modheader.m
+++ src/modheader.m
@@ -1,39 +1,42 @@
 function modheader(inputfilepath, parameter, value, varargin)
-%modheader Modify PET header metadata.
-%   Usage:
-%   Single directory: 
-%   modheader(inputfilepath, parameter, value)
-%   Recursive:
-%   modheader(inputfilepath, parameter, value, 'recursive')
-%   Note:
-%   To specify all files in a directory (regardless of extension),
-%   a trailing forwardslash is required (at least on macOS). E.g., 
-%   use '~/directory/' instead of '~/directory', the latter is
-%   interpreted as the file 'directory' in the home directory.
+%Modify PET header metadata.
+%
+%Usage: modheader(inputfilepath, parameter, value)
+%
+%Changes the parameter `parameter` to the value `value` in all the 
+%files (either given explicitly, matching whildcards, or in a
+%directory) given by `inputfilepath`.
+%Optional arguments:
+%    `recursive`: Search directories within `inputfilepath`. Valid
+%        options are true or false.
+%    Default: false
+%        `verbose`: Display files being processed. Valid options are
+%        true or false.
+%    Default: false
+%    `backupextension`: Backup original files by copying and
+%        appending this extension. Empty value `[]` does not create
+%        backups. 
+%        Default: []
 
-%%%%%% Brainstorming Notes: %%%%%
-% Mandatory arguments: 
-%   % file or path (operate on single file vs whole directory)
-%   % parameter to change (e.g, 'calibration_factor'), notify user if not
-%       found in filed
-%   % value of the parameter (e.g., '6.74419e6'), output a successfull
-%   change
-%       'file.hdr: value of calibration_factor changed from 1 to 6.74419e6'
-% Optional arguments:
-%   -r recursive (only valid for path arguments)
-%   file name filter specifier (e.g., D*.hdr vs F*.hdr for DPET vs Focus)
-%   Maybe just put in the filename, because that's how unix commands work,
-%   e.g., 'ls *.hdr' shows the header files. 
-%Recursively search all subdirectories for .hdr files
-%In hdr file, change calibration_units 0 to calibration_units 1
-%In hdr files, change calibration_factor to: 
-%If hdr file starts with "D", use DPET_calibration
-%If hdr file starts with "F", use F120_calibration
-
-
+% Sensible Defaults
 recursive = false;
-if nargin > 3 && strcmpi(varargin{1}, 'recursive')
-    recursive = true;
+verbose = false;
+backupextension = [];
+
+% Read optional arguments
+if (rem(length(varargin),2)==1)
+    error('Optional parameters should always go by pairs');
+else
+    for ii = 1:2:(length(varargin)-1)
+        switch lower(varargin{ii})
+            case 'verbose';             verbose             = varargin{ii+1};
+            case 'recursive';           recursive           = varargin{ii+1}; %
+            case 'backupextension';     backupextension     = varargin{ii+1}; %
+        otherwise
+            % Something wrong with the parameter string
+            error(['Unrecognized option: ''', varargin{ii}, '''']);
+        end
+    end
 end
 
 % Interpret 'inputfilepath'
@@ -46,13 +49,11 @@ if recursive
     basepath = textscan(genpath(basepath{1}), '%s', 'delimiter', ':');
     basepath = basepath{1};
 end
-% Left with
-% basepath: cell array of paths, no trailing '/'
-% filename: The base of the filename with wildcards, empty if unspecified.
-% extension: The extension of the file, empty if unspecified.
-basepath
-filename
-extension
+% After the above, we have the variables
+% basepath: cell array of paths, no trailing '/', 
+% filename: the base of the filename with wildcards, 
+%           empty if unspecified, and
+% extension: the extension of the file, empty if unspecified.
 
 % Do the modifications:
 % Loop over paths
@@ -67,45 +68,25 @@ for path_index = 1:length(basepath)
         if not(current_file.isdir)
             current_file_full_path = ...
                 fullfile(current_file.folder, current_file.name);
+            if verbose
+                fprintf('Processing %s\n', current_file_full_path)
+            end
+            % Read header file
+            old_header = fileread(current_file_full_path);
+            % Use regex replacement
+            new_header = regexprep(old_header, ...
+                            [newline, parameter,' \S*'], ...
+                            [parameter,' ',value]);
+            % If desired, save a backup:
+            if not(isempty(backupextension))
+                fid = fopen([current_file_full_path,'.',backupextension],'wt');
+                fprintf(fid, '%s', old_header);
+                fclose(fid);
+            end
+            % Overwrite existing
+            fid = fopen(current_file_full_path,'wt');
+            fprintf(fid, '%s', new_header);
+            fclose(fid);
         end
     end
-end
-
-
-return
-%loop over the paths list
-for i=1:length(paths)
-    %in current directory, get file list, 
-    files = dir(paths{i});
-    %loop through files in this directory
-    for j=1:length(files)
-       %only search for .hdr if isdir == 0
-       if files(j).isdir == 0
-           %look for .hdr files
-           [fp_path, fp_name, fp_ext] = fileparts(files(j).name);
-           if strcmpi(fp_ext, '.hdr') == 1
-               %display test
-               %disp(strcat(paths(i),'\',files(j).name))
-
-               %open file
-               fin = fopen(char(strcat(paths(i),filesep,files(j).name)), 'rt');
-               fout = fopen(char(strcat(paths(i),filesep,fp_name,'_CALFIXED',fp_ext)), 'w+t');
-               disp(strcat(paths(i),'\',fp_name,'_CALFIXED',fp_ext))
-               %search through file
-               while ~feof(fin)
-                   s = fgetl(fin);
-                   s = strrep(s,'calibration_units 0', 'calibration_units 1');
-                   if strcmpi(fp_name(1),'D')
-                       s = strrep(s, 'calibration_factor 1', strcat('calibration_factor ', DPET_calibration));
-                   elseif strcmpi(fp_name(1), 'F')
-                       s = strrep(s, 'calibration_factor 1', strcat('calibration_factor ', F120_calibration));
-                   end
-                   fprintf(fout,'%s\n',s);
-               end                        
-                  fin = fclose(fin);
-                  fout = fclose(fout);
-           end
-       end
-    end
-    
 end
