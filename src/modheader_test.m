8369575ab77e4ae0579fb5af64f9144e63244d91
Zachary T. Harmany <zharmany@gmail.com>
2017-08-05 00:41:52 -0500

    Fix whitespace.

--- src/modheader_test.m
+++ src/modheader_test.m
@@ -8,7 +8,6 @@ modheader('../data/F*.hdr', 'calibration_factor', '7.00806e6', ...
 % Example 2: Recursively modify all headers beginning with D
 % by changing the 'calibration_factor' field to '6.74419e6',
 % Saving backup files with appended extension .backup
-
 modheader('../data/D*.hdr', 'calibration_factor', '6.74419e6', ...
     'recursive', true, 'backupextension', 'backup')
 
