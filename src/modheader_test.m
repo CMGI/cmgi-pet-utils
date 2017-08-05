98c3ce8f4bbe3dd777b6d33bc88754cac8aa6c7a
Zachary T. Harmany <zharmany@gmail.com>
2017-08-05 00:21:38 -0500

    Fixed paths in examples, added examples of using additional features in modheader.m.

--- src/modheader_test.m
+++ src/modheader_test.m
@@ -2,13 +2,18 @@
 
 % Example 1: Recursively modify all headers beginning with F
 % by changing the 'calibration_factor' field to '7.00806e6'.
-modheader('~/data/F*.hdr', 'calibration_factor', '7.00806e6', 'recursive')
+modheader('../data/F*.hdr', 'calibration_factor', '7.00806e6', ...
+    'recursive', true)
 
 % Example 2: Recursively modify all headers beginning with D
-% by changing the 'calibration_factor' field to '6.74419e6'.
+% by changing the 'calibration_factor' field to '6.74419e6',
+% Saving backup files with appended extension .backup
 
-modheader('~/data/D*.hdr', 'calibration_factor', '6.74419e6', 'recursive')
+modheader('../data/D*.hdr', 'calibration_factor', '6.74419e6', ...
+    'recursive', true, 'backupextension', 'backup')
 
 % Example 3: Recursively modify all headers by changing
 % the 'calibration_units' field to '1'.
-modheader('~/data/*.hdr', 'calibration_units', '1', 'recursive')
+% Use verbose output
+modheader('../data/*.hdr', 'calibration_units', '1', ...
+    'recursive', true, 'verbose', true)
